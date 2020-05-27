872d1cede2ebae0719a342e68ad8ecb0066a6634
AmrShakhshir <amrshakhshir@hotmail.com>
2020-05-27 20:42:23 +0200

    first commit

---
+++ build/precache-manifest.57a0116adfd01193cf27f4e663bc027b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6527bee841e01fd498d780cd8143598b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc540b817f43646402bb",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc540b817f43646402bb",
+    "url": "/static/js/2.9b56cbf7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9b56cbf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e884dc35fe8e6580cd",
+    "url": "/static/js/main.5296576c.chunk.js"
+  },
+  {
+    "revision": "bee2be07b03bc505f706",
+    "url": "/static/js/runtime-main.2d265483.js"
+  }
+]);
