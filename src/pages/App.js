106aa2672e7ec36ac85a423e5be194edb459cfa6
AmrShakhshir <amrshakhshir@hotmail.com>
2020-06-01 20:20:53 +0200

    link to Heroku

--- src/pages/App.js
+++ src/pages/App.js
@@ -65,7 +65,7 @@ export default class App extends React.Component{
   submit = () => {
     
     axios ({
-      url: 'https://backend-part-ude-advwebtech.herokuapp.com/login',
+      url: 'https://flaskbackend-ude.herokuapp.com/login',
       method: 'POST',
       data:{
         "email": this.state.email,
